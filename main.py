1071fd54103d93203f07d446f69f2d8e09ea90ab
ReedBostic <Bosticreed@gmail.com>
2020-10-07 20:06:44 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Author: Reed Bostic
+# Date: 10/7/2020
+# Description: Ask the user for five numbers, then give them an average
+
+print("Please enter five numbers")
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+num5 = float(input())
+print("The average of those numbers is:", num1 + num2 + num3 + num4 + num5 // 5)
